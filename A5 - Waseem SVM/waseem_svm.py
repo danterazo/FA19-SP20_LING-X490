3dea19efda0fe9024e94dccf6cee599b8edae2f8
Dante Razo <dante@razonet.org>
2019-10-31 07:05:28 -0400

    A5 works (still need to optimize SVM). Commit Waseem data + update gitignore

--- A5 - Waseem SVM/waseem_svm.py
+++ A5 - Waseem SVM/waseem_svm.py
@@ -1,39 +1,71 @@
 # LING-X 490 Assignment 5: Waseem SVM
 # Dante Razo, drazo, 10/30/2019
-from sklearn.utils import shuffle
 from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 import sklearn.metrics
-import pandas as pd
+import numpy as np
 import random
 
-# import datasets and create class vector
-# ID, Diagnosis, mean features (x10), standard error features (x10), worst features (x10)
-features = "ID Diagnosis meanRadius meanTexture meanPerimeter meanArea meanSmoothness meanCompactness meanConcavity " \
-           "meanConcavePts meanSymmetry meanFractalDim seRadius seTexture sePerimeter seArea seSmoothness " \
-           "seCompactness seConcavity seConcavePts seSymmetry seFractalDim worstRadius worstTexture worstPerimeter " \
-           "worstArea worstSmoothness worstCompactness worstConcavity worstConcavePts worstSymmetry " \
-           "worstFractalDim".split()
-df = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data",
-                 names=features)
-df = shuffle(df)  # shuffle data for k-folds cross-validation
-classes = df.iloc[0:df.shape[0] + 1, 1]  # class vector from second column of data
-classes = classes.replace(['B', 'M'], [0, 1])  # replace strings with ints for validation
+# Import data
+# TODO: remove http://t.co/* links
+data_dir = "./data/"
+a = open(data_dir + "X_train", "r", encoding="utf-8")
+X_train = np.asarray(a.read().splitlines()).reshape(-1, 1)
 
-df.drop('Diagnosis', axis=1, inplace=True)  # remove class column
-df.drop('ID', axis=1, inplace=True)  # remove ID column (not needed)
+a = open(data_dir + "x_test", "r", encoding="utf-8")
+X_test = np.asarray(a.read().splitlines()).reshape(-1, 1)
 
-# k-folds cross-validation with k=5
-X_train, X_test, y_train, y_test = train_test_split(df, classes, test_size=0.8, train_size=0.2)
+a = open(data_dir + "y_train", "r", encoding="utf-8")
+y_train = a.read().splitlines()
+for i in range(0, len(y_train)):
+    y_train[i] = int(y_train[i])
 
-# Normalize data
-df = sklearn.preprocessing.normalize(df, axis=0)
+a = open(data_dir + "y_test", "r", encoding="utf-8")
+y_test = a.read().splitlines()
+for i in range(0, len(y_test)):
+    y_test[i] = int(y_test[i])
+
+a.close()
+
+# Feature engineering: One-hot encoding or Label encoding
+choice = "enc"
+if choice is "enc":
+    # combine dataset temporarily for one-hot encoding
+    X = np.concatenate((X_train, X_test))
+
+    enc = preprocessing.OneHotEncoder(handle_unknown='ignore')
+    enc_train_fit = enc.fit(X)
+    X = enc_train_fit.transform(X)
+
+    # split dataset again
+    split = X_train.shape[0]
+    X_train = X[0:split]
+    X_test = X[split:X.shape[0]]  # rest
+
+"""
+# issues w/ number of features
+if choice is "enc-old":
+    enc = preprocessing.OneHotEncoder(handle_unknown='ignore')
+    enc_train_fit = enc.fit(X_train)
+    X_train = enc_train_fit.transform(X_train)
+
+    enc_test_fit = enc.fit(X_test)
+    X_test = enc_test_fit.transform(X_test)
+
+if choice is "le":
+    le = preprocessing.LabelEncoder()
+    le_train_fit = le.fit(X_train)
+    X_train = le_train_fit.transform(X_train)
+
+    le_test_fit = le.fit(X_test)
+    X_test = le_test_fit.transform(X_test)
+"""
 
 # Fitting the model
 svm = SVC(kernel='rbf', gamma='scale')
 svm.fit(X_train, y_train)
 
 # Testing + results
-print("Random/Baseline Accuracy:",
-      sklearn.metrics.balanced_accuracy_score(y_test, [random.randint(0, 1) for x in range(1, (y_test.shape[0] + 1))]))
-print("Testing Accuracy: ", sklearn.metrics.accuracy_score(y_test, svm.predict(X_test)))
+rand_acc = sklearn.metrics.balanced_accuracy_score(y_test, [random.randint(1, 2) for x in range(0, len(y_test))])
+print(f"Random/Baseline Accuracy: {rand_acc}")
+print(f"Testing Accuracy: {sklearn.metrics.accuracy_score(y_test, svm.predict(X_test))}")
